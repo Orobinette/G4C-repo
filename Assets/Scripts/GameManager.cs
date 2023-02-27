42eaaf91c5978e687d10494d8e9b1d3ec1c36426
Orsonic13 <robinetteorson@gmail.com>
2023-02-27 15:41:58 -0800

    Script Changes
    -Added click and Drag script
    -moved old scripts to Obsolete folder
    -old GameManager -> ObsGameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,79 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    bool dragging = false;
 
-/*********
-*VARIABLES
-**********/
-
-    //Object References
-    [SerializeField] TextMeshProUGUI scoreText;
-    [SerializeField] GameObject binPrefab;
-
-    //Class References
-    [SerializeField] TruckMovement truckMovement;
-    [SerializeField] PlayerMovement playerMovement;
-
-    //Script Variables
-    public int score = 0;
-    [HideInInspector] public int game_state; //0 = drive, 1 = walk
-
-    Vector3 offset = new Vector3(-1, 0, 0); //Spawn point of player from truck 
-
-    int adjustment = 1;
-
-
-/*********
-*FUNCTIONS
-**********/
-
-    //Built-In Functions
-    void Start() 
+    public GameObject selectedObject;
+    void Update()
     {
-        game_state = 0; 
-
-        for(var i = 0; i <= 5; i++) //Spawn 5 bins
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0) && dragging == false)
         {
-            Instantiate(binPrefab, new Vector3(Random.Range(0, 5), Random.Range(0, 5), 0), Quaternion.identity);
+            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+            if (targetObject)
+            {
+                selectedObject = targetObject.transform.gameObject;
+                dragging = true;
+            }
         }
-    }
-
-    //Custom Functions
-    public void SwitchGameState(int new_state) //Switch Between Driving and Walking (parameter = the new state)
-    {
-        game_state = new_state; 
-
-        if(game_state == 0)
+        else if (dragging == true)
         {
-            //Establish Constrainss
-            truckMovement.rb.constraints = RigidbodyConstraints2D.None;
-            playerMovement.rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
-
-            //Despawn Player
-            playerMovement.playerRender.enabled = false;
-            playerMovement.playerTrans.parent = truckMovement.truckTrans;
+            dragging = false;
+            selectedObject = null;
         }
-        if(game_state == 1)
+
+        if(selectedObject != null)
         {
-            //Establish Constraints
-            truckMovement.rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezePositionX;
-            playerMovement.rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-
-            //Spawn in player
-            playerMovement.playerTrans.parent = null;
-            playerMovement.playerTrans.position = truckMovement.truckTrans.position - offset; 
-            playerMovement.playerTrans.rotation = Quaternion.identity;
-            playerMovement.playerRender.enabled = true;
+            selectedObject.transform.position = new Vector3(mousePosition);
         }
-    }
-
-    public void ChangeScore(int adjustment) 
-    {
-        score += adjustment;
-        scoreText.text = score.ToString();
     }
 }
