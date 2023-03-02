1a522d94136a479310739db3dbcf9cae098dadcd
Orsonic13 <robinetteorson@gmail.com>
2023-03-01 20:23:25 -0800

    Truck Movement + Pizza Box
    -Trucks spawn off screen, move to other side of screen, and despawn
    -Added Rigidbodies to trucks
    -added pizza box item

--- Assets/Scripts/Truck.cs
+++ Assets/Scripts/Truck.cs
@@ -11,17 +11,22 @@ public class Truck : MonoBehaviour
 
     [SerializeField] Transform trans;
     [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Rigidbody2D rb;
     Vector3 direction;
 
     Vector3 spawnPoint;
     GameObject item;
     int orientation;
 
+    Vector3 speedModifier = new Vector3(80, 80, 80);
+
     // Start is called before the first frame update
     void Start()
     {
         orientation = Random.Range(0, 4);
         spriteRenderer.sprite = spriteList[orientation];
+
+        trans.localScale = new Vector3(1f, 1f, 1f);
         trans.position = spawnPointList[orientation];
         direction = directionList[orientation];
 
@@ -37,8 +42,12 @@ public class Truck : MonoBehaviour
         {
             Instantiate(item, new Vector3(0f, 0f, 0f), Quaternion.identity);
         }
+        for(var i = 0; i < 10; i++)
+        {
+            rb.AddForce(Vector3.Scale(direction, speedModifier));
+        }
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1.5f);
         
         Destroy(this.gameObject);
     }
