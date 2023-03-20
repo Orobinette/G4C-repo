195cf980bb59250bf9bfa9268f30a1f0833fbf5c
Orsonic13 <robinetteorson@gmail.com>
2023-03-20 15:45:49 -0700

    UI Update
    -play and menu buttons that change sprite when hovered over
    -road renders behind trucks
    -custom cursor
    -button script

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Buttons : MonoBehaviour
+{
+    [SerializeField] Button button;
+
+    [SerializeField] Sprite hoverSprite;
+    [SerializeField] Sprite defaultSprite;
+
+    
+    void Update() 
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+        if (targetObject)
+        {
+            button.image.sprite = hoverSprite;
+        }
+        else
+        {
+            button.image.sprite = defaultSprite;
+        }
+        
+    }
+}
